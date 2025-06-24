49fe377de0a361f5102cd97f98aef37668b7744c
Abdallah-molla <abdallahalmalawany@gmail.com>
2025-06-25 02:55:36 +0300

    fix pylint warnings

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# pylint: disable=import-error
+"""This module sets up a FastAPI application with basic endpoints."""
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,5 +7,5 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """Fetch the current average temperature from openSenseMap API."""
     return {"Hello": "World"}
-    
